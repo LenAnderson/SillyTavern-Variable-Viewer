d168eb7f74f326a7197b1cd1e7662aa90806aa7f
LenAnderson <Anderson.Len@outlook.com>
2023-12-07 22:10:16 +0000

    some redesign

---
+++ src/Variable.js
@@ -0,0 +1,89 @@
+import { executeSlashCommands } from "../../../../slash-commands.js";
+import { delay } from "../../../../utils.js";
+
+export class Variable {
+	/**@type {String}*/ name;
+	/**@type {any}*/ value;
+	/**@type {Boolean}*/ global;
+	
+	/**@type {HTMLElement}*/ dom;
+	/**@type {HTMLInputElement}*/ domValue;
+
+
+
+
+	constructor(/**@type {String}*/name, /**@type {any}*/value, /**@type {Boolean}*/global) {
+		this.name = name;
+		this.value = value;
+		this.global = global;
+	}
+
+
+
+
+	render() {
+		if (!this.dom) {
+			let ta;
+			const v = document.createElement('div'); {
+				this.dom = v;
+				v.classList.add('vv--var');
+				v.classList.add('vv--add');
+				const head = document.createElement('div'); {
+					head.classList.add('vv--head');
+					const lbl = document.createElement('div'); {
+						lbl.classList.add('vv--label');
+						lbl.textContent = this.name;
+						head.append(lbl);
+					}
+					const actions = document.createElement('div'); {
+						actions.classList.add('vv--actions');
+						const flush = document.createElement('span'); {
+							flush.classList.add('vv--action');
+							flush.textContent = '🚽';
+							flush.title = 'Flush variable';
+							flush.addEventListener('click', ()=>{
+								executeSlashCommands(`/flush${this.global?'global':''}var ${this.name}`);
+							});
+							actions.append(flush);
+						}
+						const save = document.createElement('span'); {
+							save.classList.add('vv--action');
+							save.textContent = '💾';
+							save.title = 'Set variable value';
+							save.addEventListener('click', ()=>{
+								executeSlashCommands(`/set${this.global?'global':''}var this.name="${this.name}" ${ta.value}`);
+							});
+							actions.append(save);
+						}
+						head.append(actions);
+					}
+					v.append(head);
+				}
+				const val = document.createElement('div'); {
+					val.classList.add('vv--val');
+					ta = document.createElement('textarea'); {
+						this.domValue = ta;
+						ta.value = this.value;
+						val.append(ta);
+					}
+					v.append(val);
+				}
+			}
+		}
+		return this.dom;
+	}
+
+	async update(newVal) {
+		this.dom.classList.remove('vv--add');
+		this.value = newVal;
+		this.domValue.value = this.value;
+		this.dom.classList.add('vv--update');
+		await delay(2000);
+		this.dom.classList.remove('vv--update');
+	}
+	async remove() {
+		this.dom.classList.add('vv--remove');
+		await delay(2000);
+		this.dom.remove();
+	}
+}
--- src/VariableViewer.js
+++ src/VariableViewer.js
@@ -2,6 +2,7 @@ import { callPopup, chat_metadata } from "../../../../../script.js";
 import { extension_settings, getContext } from "../../../../extensions.js";
 import { executeSlashCommands } from "../../../../slash-commands.js";
 import { delay } from "../../../../utils.js";
+import { Variable } from "./Variable.js";
 
 export class VariableViewer {
 	/**@type {Boolean}*/ isShown = true;
@@ -9,9 +10,14 @@ export class VariableViewer {
 
 	/**@type {HTMLElement}*/ dom;
 	/**@type {HTMLElement}*/ content;
+	/**@type {HTMLElement}*/ localPanel;
+	/**@type {HTMLElement}*/ globalPanel;
 
 	/**@type {String}*/ oldLocalVars;
 	/**@type {String}*/ oldGlobalVars;
+
+	/**@type {Variable[]}*/ localVarList = [];
+	/**@type {Variable[]}*/ globalVarList = [];
 
 
 
@@ -51,87 +57,45 @@ export class VariableViewer {
 				const content = document.createElement('div'); {
 					this.content = content;
 					content.classList.add('vv--content');
+					[['Local', false], ['Global', true]].forEach(([panelTitle,global])=>{
+						const panel = document.createElement('div'); {
+							panel.classList.add('vv--entry');
+							const title = document.createElement('div'); {
+								title.classList.add('vv--title');
+								title.textContent = panelTitle;
+								const add = document.createElement('div'); {
+									add.classList.add('vv--add');
+									add.textContent ='➕';
+									add.title = 'Add new variable';
+									add.addEventListener('click', async()=>{
+										const name = await callPopup('Variable Name', 'input', '', {okButton:'OK', rows:1, wide:false, large:false});
+										if (!name) {
+											return;
+										}
+										const val = await callPopup('Variable Value', 'input', '', {okButton:'OK', rows:1, wide:false, large:false});
+										if (val === null || val === undefined) {
+											return;
+										}
+										executeSlashCommands(`/set${global?'global':''}var key="${name}" ${val}`);
+									});
+									title.append(add);
+								}
+								panel.append(title);
+							}
+							const vars = document.createElement('div'); {
+								this[`${global?'global':'local'}Panel`] = vars;
+								vars.classList.add('vv--vars');
+								panel.append(vars);
+							}
+							this.content.append(panel);
+						}
+					});
 					root.append(content);
 				}
 			}
 		}
 		document.body.append(this.dom);
 		this.update();
-	}
-
-	renderVariables(panelTitle, variables, global) {
-		const panel = document.createElement('div'); {
-			panel.classList.add('vv--entry');
-			const title = document.createElement('div'); {
-				title.classList.add('vv--title');
-				title.textContent = panelTitle;
-				const add = document.createElement('div'); {
-					add.classList.add('vv--add');
-					add.textContent ='➕';
-					add.title = 'Add new variable';
-					add.addEventListener('click', async()=>{
-						const name = await callPopup('Variable Name', 'input', '', {okButton:'OK', rows:1, wide:false, large:false});
-						if (!name) {
-							return;
-						}
-						const val = await callPopup('Variable Value', 'input', '', {okButton:'OK', rows:1, wide:false, large:false});
-						if (val === null || val === undefined) {
-							return;
-						}
-						executeSlashCommands(`/set${global?'global':''}var key="${name}" ${val}`);
-					});
-					title.append(add);
-				}
-				panel.append(title);
-			}
-			const vars = document.createElement('div'); {
-				vars.classList.add('vv--vars');
-				Object.keys(variables).forEach(key=>{
-					let ta;
-					const v = document.createElement('div'); {
-						v.classList.add('vv--var');
-						const lbl = document.createElement('div'); {
-							lbl.classList.add('vv--label');
-							lbl.textContent = key;
-							v.append(lbl);
-						}
-						const val = document.createElement('div'); {
-							val.classList.add('vv--val');
-							ta = document.createElement('textarea'); {
-								ta.value = variables[key];
-								val.append(ta);
-							}
-							v.append(val);
-						}
-						const actions = document.createElement('div'); {
-							actions.classList.add('vv--actions');
-							const flush = document.createElement('span'); {
-								flush.classList.add('vv--action');
-								flush.textContent = '🚽';
-								flush.title = 'Flush variable';
-								flush.addEventListener('click', ()=>{
-									executeSlashCommands(`/flush${global?'global':''}var ${key}`);
-								});
-								actions.append(flush);
-							}
-							const save = document.createElement('span'); {
-								save.classList.add('vv--action');
-								save.textContent = '💾';
-								save.title = 'Set variable value';
-								save.addEventListener('click', ()=>{
-									executeSlashCommands(`/set${global?'global':''}var key="${key}" ${ta.value}`);
-								});
-								actions.append(save);
-							}
-							v.append(actions);
-						}
-						vars.append(v);
-					}
-				});
-				panel.append(vars);
-			}
-			this.content.append(panel);
-		}
 	}
 
 
@@ -149,12 +113,44 @@ export class VariableViewer {
 				this.oldLocalVars = lv;
 				this.oldGlobalVars = gv;
 				console.log('[STVV]', localVars, globalVars);
-				this.content.innerHTML = '';
-				this.renderVariables('Local Variables', localVars, false);
-				this.renderVariables('Global Variables', globalVars, true);
+				this.updateVars(this.localVarList, localVars, this.localPanel, false);
+				this.updateVars(this.globalVarList, globalVars, this.globalPanel, true);
 			}
 			await delay(200);
 		}
 		this.isRunning = false;
+	}
+
+
+	updateVars(oldVars, newVars, container, global) {
+		newVars = Object.keys(newVars).map(key=>new Variable(key, newVars[key], global)).toSorted((a,b)=>a.name.localeCompare(b.name));
+		
+		const toRemove = oldVars.filter(o=>!newVars.find(n=>o.name==n.name));
+		toRemove.forEach(it=>it.remove());
+		for (let i=oldVars.length-1; i>=0; i--) {
+			const item = oldVars[i];
+			if (toRemove.includes(item)) {
+				oldVars.splice(i, 1);
+			}
+		}
+
+		const toUpdate = oldVars.filter(o=>o.value!=newVars.find(n=>o.name==n.name).value);
+		toUpdate.forEach(o=>o.update(newVars.find(n=>n.name==o.name).value));
+		
+		const toAdd = newVars.filter(n=>!oldVars.find(o=>n.name==o.name));
+		oldVars.push(...toAdd);
+		oldVars.sort((a,b)=>a.name.localeCompare(b.name));
+		let prev;
+		for (let i=oldVars.length-1; i>=0; i--) {
+			const item = oldVars[i];
+			if (toAdd.includes(item)) {
+				if (prev) {
+					prev.dom.insertAdjacentElement('beforebegin', item.render());
+				} else {
+					container.append(item.render());
+				}
+			}
+			prev = item;
+		}
 	}
 }
